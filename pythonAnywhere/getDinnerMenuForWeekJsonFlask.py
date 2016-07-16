35f78cce97abadf59b92de032598517834085679
Kreloc <kingpincat@hotmail.com>
2016-07-15 16:23:43 -0800

    Updated getDinnerMenu flask app
    Updated dinner menu app to exclude the previous week selection of
    dinners.

--- pythonAnywhere/getDinnerMenuForWeekJsonFlask.py
+++ pythonAnywhere/getDinnerMenuForWeekJsonFlask.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 import random
+import os
 import json
 import datetime
+import time
+import calendar
 app = Flask(__name__)
 app.secret_key = 'This is really unique and secret'
 
@@ -9,9 +12,6 @@ app.secret_key = 'This is really unique and secret'
 #! python3
 # getDinnerSelection - Gets dinner selection for the week from list in script
 
-##import random
-##import os
-##import json
 #Dictionary of dinners and their ingeredients
 @app.route('/')
 def getDinnerMenu():
@@ -22,8 +22,63 @@ def getDinnerMenu():
     #Define dinner options
     dinner_ingredients = json_data
     dinner_options = list(json_data.keys())
+    #TODO: Determine week number
+    now = datetime.datetime.now()
+    calendar.setfirstweekday(6)
+    weeks = calendar.monthcalendar(now.year, now.month)
+    for x in range(len(weeks)):
+        if now.day in weeks[x]:
+            week_number = x+1    
+    #Parse text file(s) based on weeknumber and remove dinner options found from the past
+    #week's menu from dinner options.    
+    if week_number == 1:
+        if os.path.isfile('/home/Kreloc/mysite/static/menus/dinner_week_five.txt') == True:
+            dinner_file = open('/home/Kreloc/mysite/static/menus/dinner_week_five.txt')
+            dinner_choices = (dinner_file.readline()).split(',')
+            for dinner_choice in dinner_choices:
+                dinner_options.remove(dinner_choice)
+            dinner_file.close()
+        if os.path.isfile('/home/Kreloc/mysite/static/menus/dinner_week_four.txt') == True:
+            dinner_file = open('/home/Kreloc/mysite/static/menus/dinner_week_four.txt')
+            dinner_choices = (dinner_file.readline()).split(',')
+            for dinner_choice in dinner_choices:
+                dinner_options.remove(dinner_choice)
+            dinner_file.close()
+    if week_number == 2:
+        if os.path.isfile('/home/Kreloc/mysite/static/menus/dinner_week_one.txt') == True:
+            dinner_file = open('/home/Kreloc/mysite/static/menus/dinner_week_one.txt')
+            dinner_choices = (dinner_file.readline()).split(',')
+            for dinner_choice in dinner_choices:
+                dinner_options.remove(dinner_choice)
+            dinner_file.close()        
+    if week_number == 3:
+        if os.path.isfile('/home/Kreloc/mysite/static/menus/dinner_week_one.txt') == True:
+            dinner_file = open('/home/Kreloc/mysite/static/menus/dinner_week_one.txt')
+            dinner_choices = (dinner_file.readline()).split(',')
+            for dinner_choice in dinner_choices:
+                dinner_options.remove(dinner_choice)
+            dinner_file.close()
+        if os.path.isfile('/home/Kreloc/mysite/static/menus/dinner_week_two.txt') == True:
+            dinner_file = open('/home/Kreloc/mysite/static/menus/dinner_week_two.txt')
+            dinner_choices = (dinner_file.readline()).split(',')
+            for dinner_choice in dinner_choices:
+                dinner_options.remove(dinner_choice)
+            dinner_file.close()
+    if week_number == 4:
+        if os.path.isfile('/home/Kreloc/mysite/static/menus/dinner_week_three.txt') == True:
+            dinner_file = open('/home/Kreloc/mysite/static/menus/dinner_week_three.txt')
+            dinner_choices = (dinner_file.readline()).split(',')
+            for dinner_choice in dinner_choices:
+                dinner_options.remove(dinner_choice)
+            dinner_file.close()              
+    if week_number == 5:
+        if os.path.isfile('/home/Kreloc/mysite/static/menus/dinner_week_four.txt') == True:
+            dinner_file = open('/home/Kreloc/mysite/static/menus/dinner_week_four.txt')
+            dinner_choices = (dinner_file.readline()).split(',')
+            for dinner_choice in dinner_choices:
+                dinner_options.remove(dinner_choice)
+            dinner_file.close()      
 
-    #TODO: Parse text file(s) and remove dinner options found from the past
     all_ingredients = []
     dinner_list = []
     random_leftover_number = random.randint(4, 6)
@@ -47,7 +102,7 @@ def getDinnerMenu():
     #Loop thru dinner_list, assign to days, and get ingredients needed
     #print week menu plan
     dinner_menu = []
-    final_output.append('Dinners this week')
+    final_output.append('Dinners this week:')
     days_of_week = (
         "Monday: ",
         "Tuesday: ",
@@ -73,10 +128,15 @@ def getDinnerMenu():
     #final_output.append(all_ingredients)
     for ingredient_list in all_ingredients:
         final_output.append(ingredient_list)
-    now = datetime.datetime.now()
     date_string = ('Menu generated on ' + (str(now.month)) + '-' + (str(now.day)) + '-' + str((now.year)))
     final_output.append(date_string)
     #TODO: Format as shopping list with number of each item
     #TODO: Output text file for each week
+    if week_number == 3:
+        #Remove leftovers from list using list comprhension]
+        dinner_list = [x for x in dinner_list if x != 'Leftovers']
+        dinner_file = open('/home/Kreloc/mysite/static/menus/dinner_week_three.txt', 'w')
+        dinner_file.write(','.join(dinner_list))
+        dinner_file.close()    
     ##    #TODO: Remove past dinner text files
     return render_template('index.html', pages=final_output)
